088bf1635935dca7635f00fb176588eb79e4cce7
Ramk <prabhuraju06@gmail.com>
2019-05-27 23:34:44 +0530

    added three screen and modified some UI

---
+++ Torque/CalculationMethod.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Torque
+{
+    /// <summary>
+    /// Interaction logic for CalculationMethod.xaml
+    /// </summary>
+    public partial class CalculationMethod : Window
+    {
+        public CalculationMethod()
+        {
+            InitializeComponent();
+        }
+
+        private void RadioButton_Checked(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ Torque/ProjectDetails.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Torque
+{
+    /// <summary>
+    /// Interaction logic for ProjectDetails.xaml
+    /// </summary>
+    public partial class ProjectDetails : Window
+    {
+        public ProjectDetails()
+        {
+            InitializeComponent();
+        }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+
+        }
+    }
+}
