a04ae4925e244d42101556d039539e9ae7d585dc
Ramk <prabhuraju06@gmail.com>
2019-05-28 00:35:59 +0530

    Fastener,tightening condition,Loading Joint screen UI

---
+++ Torque/TighteningConditions.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Torque
+{
+    /// <summary>
+    /// Interaction logic for TighteningConditions.xaml
+    /// </summary>
+    public partial class TighteningConditions : Window
+    {
+        public TighteningConditions()
+        {
+            InitializeComponent();
+        }
+
+        private void CheckBox_Checked(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void CheckBox_Checked_1(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
